3541fa9973ffb47846204a18916e1ddb4b18aacd
Alexandros Lotsos <alex@unseenmedia.io>
2019-04-07 16:19:30 -0400

    fixed typeof boolean expression

--- demos/tap-place.js
+++ demos/tap-place.js
@@ -12,10 +12,10 @@ AFRAME.registerComponent('tap-place', {
     //get the main logo and ground in the scene
     var mainLogo = document.getElementById('mainLogo')
     var theGround = document.getElementById('ground')
-    console.log(mainLogo)
+    console.log(typeof(mainLogo)
 
     //if mainLogo isn't null then remove this component from the scene
-    if (typeof (mainLogo) != null) {
+    if (typeof (mainLogo) != "null") {
       theGround.removeEventListener('click', this.spawner)
       //this.sceneEl.removeAttribute('tap-place')
     }
