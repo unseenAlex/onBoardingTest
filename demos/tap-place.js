f42d289b8dd2c7d095fe897bb87270368000d6f5
Alexandros Lotsos <alex@unseenmedia.io>
2019-04-07 14:30:18 -0400

    Added gesture detection to spawned entities v6

--- demos/tap-place.js
+++ demos/tap-place.js
@@ -4,37 +4,35 @@
 
 // Component that places trees where the ground is clicked
 AFRAME.registerComponent('tap-place', {
-    init: function() {
-      const ground = document.getElementById('ground')
-      ground.addEventListener('click', event => {
-        // Create new entity for the new object
-        const newElement = document.createElement('a-entity')
-        console.log(newElement)
-        // The raycaster gives a location of the touch in the scene
-        const touchPoint = event.detail.intersection.point
-        console.log(touchPoint) //me
-        newElement.setAttribute('position', touchPoint)
-        const randomYRotation = Math.random() * 360
-        newElement.setAttribute('rotation', '0 ' + randomYRotation + ' 0')
-        newElement.setAttribute('visible', 'false')
-        newElement.setAttribute('scale', '0.0001 0.0001 0.0001')
-        newElement.setAttribute('gltf-model', '#logo')
-        newElement.className += "cantap"
-        newElement.setAttribute('hold-drag', '')
-        newElement.setAttribute('pinch-scale', '')
-        newElement.setAttribute('two-finger-spin', '')
-        this.el.sceneEl.appendChild(newElement)
-        console.log(newElement)
-        newElement.addEventListener('model-loaded', () => {
-          // Once the model is loaded, we are ready to show it popping in using an animation
-          newElement.setAttribute('visible', 'true')
-          newElement.setAttribute('animation', {
-            property: 'scale',
-            to: '1 1 1',
-            easing: 'easeOutElastic',
-            dur: 800,
-          })
+  init: function () {
+    const ground = document.getElementById('ground')
+    ground.addEventListener('click', event => {
+      // Create new entity for the new object
+      const newElement = document.createElement('a-entity')
+      console.log(newElement)
+      // The raycaster gives a location of the touch in the scene
+      const touchPoint = event.detail.intersection.point
+      newElement.setAttribute('position', touchPoint)
+      const randomYRotation = Math.random() * 360
+      newElement.setAttribute('rotation', '0 ' + randomYRotation + ' 0')
+      newElement.setAttribute('visible', 'false')
+      newElement.setAttribute('scale', '0.0001 0.0001 0.0001')
+      newElement.setAttribute('gltf-model', '#logo')
+      newElement.className += "cantap"
+      this.el.sceneEl.appendChild(newElement)
+      newElement.setAttribute('hold-drag', '')
+      newElement.setAttribute('pinch-scale', '')
+      newElement.setAttribute('two-finger-spin', '')
+      newElement.addEventListener('model-loaded', () => {
+        // Once the model is loaded, we are ready to show it popping in using an animation
+        newElement.setAttribute('visible', 'true')
+        newElement.setAttribute('animation', {
+          property: 'scale',
+          to: '1 1 1',
+          easing: 'easeOutElastic',
+          dur: 800,
         })
       })
-    }
-  })
+    })
+  }
+})
